nuxleus/cherokee-webserver

commit 7add9dc8edfbf432888d069bfa23c3c0be50e24e

Some errors were not being logged in the log file.
git-svn-id: svn://svn.cherokee-project.com/cherokee/trunk@5786 5dc97367-97f1-0310-9951-d761b3857238

cherokee/connection.c
@@ -1984,6 +1984,11 @@ cherokee_connection_get_request (cherokee_connection_t *conn)
 
 error:
 	conn->error_code = error_code;
+
+	/* Since the request could not be parsed, the connection is
+	 * about to be closed. Log the error now before it's too late.
+	 */
+	cherokee_logger_write_access (CONN_VSRV(conn)->logger, conn);
 	return ret_error;
 }
 

cherokee/logger_ncsa.c
@@ -223,10 +223,16 @@ build_log_string (cherokee_logger_ncsa_t *logger,
 	/* Get the method and version strings
 	 */
 	ret = cherokee_http_method_to_string (cnt->header.method, &method, &method_len);
-	if (unlikely(ret < ret_ok)) return ret;
+	if (unlikely (ret < ret_ok)) {
+		method     = "";
+		method_len = 0;
+	}
 
 	ret = cherokee_http_version_to_string (cnt->header.version, &version, &version_len);
-	if (unlikely(ret < ret_ok)) return ret;
+	if (unlikely (ret < ret_ok)) {
+		version     = "";
+		version_len = 0;
+	}
 
 	/* Build the log string
 	 *

commit a8b59681380a9edd7a00972fa7e3a933f27594b5

Fixes a regression introduced in r5786: http://svn.cherokee-project.com/changeset/5786
git-svn-id: svn://svn.cherokee-project.com/cherokee/trunk@5814 5dc97367-97f1-0310-9951-d761b3857238

cherokee/connection.c
@@ -1988,7 +1988,9 @@ error:
 	/* Since the request could not be parsed, the connection is
 	 * about to be closed. Log the error now before it's too late.
 	 */
-	cherokee_logger_write_access (CONN_VSRV(conn)->logger, conn);
+	if (CONN_VSRV(conn)->logger) {
+		cherokee_logger_write_access (CONN_VSRV(conn)->logger, conn);
+	}
 	return ret_error;
 }
 